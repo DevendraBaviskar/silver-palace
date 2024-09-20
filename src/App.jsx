6a2268295ea1159119ffcd112e5f2b684b77cb72
Devendra <devendrabaviskar22@gmail.com>
2024-09-20 16:53:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <img src="/Images/Building.jpg" alt="" />
+      <img src="/Images/Lobby.jpg" alt="" />
+    </div>
+  );
+}
+
+export default App;
